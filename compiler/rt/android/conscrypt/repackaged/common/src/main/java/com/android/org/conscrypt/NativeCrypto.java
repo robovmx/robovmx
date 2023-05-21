e64b578a315c73ca3e4c7f14f449a099c16e6ffb
dkimitsa <demyan.kimitsa@gmail.com>
2023-05-21 12:30:10 +0300

    * Libcore 14: Conscrypt: Applied back RoboVM changes (un-patch commit reverted) re-generated native constants: > cd rt/android/cmake/conscrypt/ > ./generate_constants.sh

--- compiler/rt/android/conscrypt/repackaged/common/src/main/java/com/android/org/conscrypt/NativeCrypto.java
+++ compiler/rt/android/conscrypt/repackaged/common/src/main/java/com/android/org/conscrypt/NativeCrypto.java
@@ -57,13 +57,17 @@ import javax.security.auth.x500.X500Principal;
  * @hide This class is not part of the Android public SDK API
  */
 @Internal
+// RoboVM note: there is indirect usage in JNI, to reduce forceLinkClass entries
+@org.robovm.rt.annotation.ForceLinkClass(ConscryptForceLinkClasses.class)
 public final class NativeCrypto {
     // --- OpenSSL library initialization --------------------------------------
     private static final UnsatisfiedLinkError loadError;
     static {
         UnsatisfiedLinkError error = null;
         try {
-            NativeCryptoJni.init();
+            // RoboVM Note: library is statically linked, just call onload()
+            onload();
+//            NativeCryptoJni.init();
             clinit();
         } catch (UnsatisfiedLinkError t) {
             // Don't rethrow the error, so that we can later on interrogate the
@@ -73,6 +77,8 @@ public final class NativeCrypto {
         loadError = error;
     }
 
+    // RoboVM Note: added onload() for initialization
+    private native static void onload();
     private native static void clinit();
 
     /**
